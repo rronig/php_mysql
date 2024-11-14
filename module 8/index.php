c1f1283fa2cfbc0ed27bc9a3aa1daaf1ecdb8c0b
rronig <gashi.1rron@gmail.com>
2024-11-14 19:06:04 +0100

    changed module 8

--- module 8/index.php
+++ module 8/index.php
@@ -1,16 +1,21 @@
 <?php
-
 $host = 'localhost';
 $user = 'root';
 $pass = '';
-$db="db";
+$db = "testdb";
 
 try{
-    $pdo= new PDO("mysql:host=$host; dbname=db", $user, $pass);
-    $sql="CREATE DATABASE users (id INT(6) NOT NULL AUTO_INCREMENT PRIMARY KEY, usernam VARCHAR(30) NOT NULL, password VARCHAR(30) NOT NULL)";
-    $pdo -> exec($sql);
-    echo"Table is created!";
+    $pdo = new PDO("mysql:host=$host; dbname=$db", $user, $pass);
+
+    //$sql = "CREATE TABLE users (id INT(6) NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+    //username VARCHAR(30) NOT NULL,
+    //password VARCHAR(30) NOT NULL )";
+
+    //$pdo -> exec($sql);
+
+    //echo "Table created successfully";
+
 }catch(Exception $e){
-    echo "Table not created, something went wrong". $e->getMessage();
+    //echo "Error creating table:  ". $e->getMessage();
 }
 ?> 
